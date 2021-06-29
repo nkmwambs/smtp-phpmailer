817d2f7301f12269335810a339f19fe32fd3b97b
Onduso <londuso@us.ci.org>
2021-06-29 10:50:45 +0300

    Igonaring the email smtp setting credentials

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-use PHPMailer\PHPMailer\PHPMailer;
+
 //use PHPMailer\PHPMailer\Exception;
 
 class Welcome extends CI_Controller {
@@ -22,51 +22,17 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		require 'vendor/autoload.php';
-       // require 'vendor/PHPMailer/src/Exception.php';
-       //require 'vendor/PHPMailer/src/PHPMailer.php';
-       // require 'vendor/PHPMailer/src/SMTP.php';
+		$this->load->library('Email_library');
 
-		$mail = new PHPMailer();
+		$this->email_library->do_email('londuso@ke.ci.org', 'Onduso');
+		$this->email_library->do_email('nkarisa@ke.ci.org', 'karisa');
+		$this->email_library->do_email('gkimani@ke.ci.org', 'Kimani');
+		$this->email_library->do_email('jmuchori@ke.ci.org', 'Muchori');
+		$this->email_library->do_email('nkmwambs@gmail.com', 'Karisa');
+		$this->email_library->do_email('livingstoneonduso@gmail.com', 'Onduso');
+		$this->email_library->do_email('geogakim@gmail.com', 'Kimani');
 
-		try {
-			//Server settings
-			//$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-			$mail->isSMTP();                                            //Send using SMTP
-			$mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
-			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-			$mail->Username   = 'afrstaffrecognition@ke.ci.org';//'gtsafrdevteam';                     //SMTP username
-			$mail->Password   = 'Comp@ss1on@321!!@**@';//'@Compassion123';                               //SMTP password
-			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-			$mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 		
-			//Recipients
-			$mail->setFrom('afrstaffrecognition@ke.ci.org', 'Mailer');
-			$mail->addAddress('livingstoneonduso@gmail.com', 'Onduso');     //Add a recipient
-			$mail->addAddress('londuso@ke.ci.org');   //Name is optional
-			$mail->addAddress('nkmwambs@gmail.com'); //Name is optional
-			$mail->addAddress('nkarisa@ke.ci.org');    //Name is optional
-			$mail->addAddress('jmuchori@ke.ci.org'); //Name is optional
-			$mail->addAddress('gkimani@ke.ci.org');  //Name is optional
-			//$mail->addReplyTo('info@example.com', 'Information');
-			//$mail->addCC('cc@example.com');
-			//$mail->addBCC('bcc@example.com');
-		
-			//Attachments
-			//$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-			//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-		
-			//Content
-			$mail->isHTML(true);                                  //Set email format to HTML
-			$mail->Subject = 'Testing PhpMailer';
-			$mail->Body    = 'Recieve EMAIL From <b>Amazon Web Services</b>';
-			$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-		
-			$mail->send();
-			echo 'Message has been sent';
-		} catch (Exception $e) {
-			echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-		}
 
 		$this->load->view('welcome_message');
 	}
